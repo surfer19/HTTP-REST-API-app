0b37e0cccfdb2de3f27e89422c30768d4b41ba09
Marián Mrva <marian.mrva@cognito.cz>
2017-03-19 18:26:18 +0100

    debug

--- client.cpp
+++ client.cpp
@@ -208,8 +208,7 @@ int main (int argc, const char * argv[]) {
 
     printf("[CLIENT] RESPONSE from server: %s \n", buf);
     cout << "vazne to tu spadne????" << endl;
-    string s_header;
-    s_header.append(buf);
+    string s_header = buf;
 
     string s_helper = "";
     string s_error = "";
